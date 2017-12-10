4429819026c43bc4d78ebfe2e6b34df3544009f7
Carlos Alberto <betocac@gmail.com>
2017-12-10 17:47:17 -0200

    Não funcionou a conexão com a classe SQL

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit0f1e6bc34dd2118e46784c6c29cac0ef
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInit0f1e6bc34dd2118e46784c6c29cac0ef
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInit0f1e6bc34dd2118e46784c6c29cac0ef::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit0f1e6bc34dd2118e46784c6c29cac0ef::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit0f1e6bc34dd2118e46784c6c29cac0ef::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit0f1e6bc34dd2118e46784c6c29cac0ef::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit0f1e6bc34dd2118e46784c6c29cac0ef::$classMap;
 
